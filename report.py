ca3f4cba0a280bf33ce75220c33b35001a58aef7
gitzart <cracker1510@gmail.com>
2018-04-02 14:16:29 +0630

    Query the top three authors

--- report.py
+++ report.py
@@ -37,3 +37,23 @@ def top_articles(cursor, limit=3):
     cursor.execute(query, (limit,))
     data = cursor.fetchall()
     pprint(data)
+
+
+@connect
+def top_authors(cursor, limit=3):
+    query = '''
+        SELECT authors.name, sum(views)::int
+        FROM authors, (
+            SELECT articles.author, count(log.path) AS views
+            FROM articles
+            LEFT JOIN log
+            ON articles.slug = split_part(log.path, '/', 3)
+            GROUP BY articles.id
+        ) AS top_articles
+        WHERE authors.id = top_articles.author
+        GROUP BY authors.id
+        LIMIT %s;
+    '''
+    cursor.execute(query, (limit,))
+    data = cursor.fetchall()
+    pprint(data)
