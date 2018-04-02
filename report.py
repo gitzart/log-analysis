7396b32b2f87e5d487fad0d1254c7b15b0b4d338
gitzart <cracker1510@gmail.com>
2018-04-02 14:15:30 +0630

    Query the top three articles

---
+++ report.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+
+from functools import wraps
+from pprint import pprint
+
+import psycopg2
+
+
+DSN = os.getenv('DATABASE_URL')
+
+
+def connect(f):
+    '''Manage database connection and cursor.'''
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        conn = psycopg2.connect(DSN)
+        cursor = conn.cursor()
+        f(cursor, *args, **kwargs)
+        cursor.close()
+        conn.close()
+    return wrapper
+
+
+@connect
+def top_articles(cursor, limit=3):
+    query = '''
+        SELECT articles.title, count(log.path) AS views
+        FROM articles
+        LEFT JOIN log
+        ON articles.slug = split_part(log.path, '/', 3)
+        GROUP BY articles.id
+        ORDER BY views DESC
+        LIMIT %s;
+    '''
+    cursor.execute(query, (limit,))
+    data = cursor.fetchall()
+    pprint(data)
